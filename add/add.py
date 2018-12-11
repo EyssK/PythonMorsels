7600a833e04c24d857c788540442f4c647071efb
kabutopia <eyssartk@gmail.com>
2018-12-11 19:14:12 +0100

    add finished

--- add/add.py
+++ add/add.py
@@ -1,20 +1,46 @@
+# -*- coding: utf-8 -*-
 def add(a,*b):
-    #print("add"+str(a)+str(b))
-    return(add2(a,*b))
-#    else :
-#        return(add2(a,add_multiple(b[0],*b[1:])))
+    """add any number of matrices of the same dimension 
+        with respect to the indexes
+
+    Args:
+        a (list): first matrix
+        b : variable length of matrices
+
+    Raises:
+        ValueError: if input matrices doesn't have the same dimension
+    """
+    if len(b) == 1:
+        return(add2(a,*b))
+    else :
+        return(add2(a,add(b[0],*b[1:])))
+
+
 
 def add2(a,b):
+    """add 2 matrices of the same dimension
     
-    # works only dor depth 1 lists
-    # return [ x+y for (x,y) in zip(a,b) ]
+    Args:
+        a (list): first matrix
+        b (list): second matrix
 
-    #print("add2"+str(a)+str(b))
+    Return:
+        list: The addition of the 2 matrices
+
+    Raises:
+        ValueError: if a and b doesn't have the same dimension
+
+    """
+    if len(a) != len(b):
+        raise ValueError
 
     l = list()
-    for (x,y) in zip(a,b):
+    for x,y in zip(a,b):
         if type(x) == type(tuple()) or type(x) == type(list()):
             l.append( add2(x,y))
         else:
             l.append(x+y)
     return(l)
+
+
+
--- count_words/count.py
+++ count_words/count.py
@@ -1,16 +1,22 @@
 
+def getOnlyLetters(s):
+    if s[0].isalpha() == False :
+        return getOnlyLetters(s[1:])
+    elif s[-1].isalpha() == False :
+        return getOnlyLetters(s[:-1])
+    else: 
+        return s
 
 def count_words(s):
     D = dict()
     L = s.split(' ')
     for _w in L:
         w = _w.lower()
+        w = getOnlyLetters(w)
         if w in D:
             D[w] = D[w] + 1
         else:
             D[w]=1
     
     return D
-
-
 
