f7143aa222f7c54a0d093eb6672456154a8281c5
kabutopia <eyssartk@gmail.com>
2018-12-12 20:30:26 +0100

    add 2nd version

--- count_words/count.py
+++ count_words/count.py
@@ -9,14 +9,40 @@ def getOnlyLetters(s):
 
 def count_words(s):
     D = dict()
-    L = s.split(' ')
-    for _w in L:
-        w = _w.lower()
+    for w in s.lower().split():
         w = getOnlyLetters(w)
         if w in D:
-            D[w] = D[w] + 1
+            D[w] += 1
         else:
             D[w]=1
     
     return D
 
+
+
+# Other solution :
+if __name__ == "__main__":
+    
+    from collections import Counter
+
+    def test(a,b):
+        if a==b:
+            print("OK")
+        else:
+            raise ValueError("Test Failed\n"+ str(a) + " != " + str(b))
+
+    def count_words(s):
+        return Counter(s.lower().split())
+
+
+    actual = count_words("oh what a day what a lovely day")
+    expected = {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+    test(actual,expected)
+
+    actual = count_words("don't stop believing")
+    expected = {"don't": 1, 'stop': 1, 'believing': 1}
+    test(actual,expected)
+
+    actual = count_words("Oh what a day what a lovely day")
+    expected = {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+    test(actual,expected)
